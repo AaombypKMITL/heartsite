541717eca707b1acc944c0f03b8be6717a657581
aomyompakdee <aaomaaom.5555@gmail.com>
2024-04-20 12:58:50 +0700

    test

---
+++ heartsite/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="w-full h-screen flex justify-center items-center">
+    <div class="flex justify-center items-center content-center">
+        <h1 class="text-center text-3xl"><b>print("Hello World!")</b></h1>
+        
+    </div>
+    
+</div>
